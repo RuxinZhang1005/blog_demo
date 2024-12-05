bb5fd3747a5e82a10ff2eeb3bab4d32bf1b224a1
RuxinZhang1005 <38184231+RuxinZhang1005@users.noreply.github.com>
2024-12-05 16:30:41 +0100

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,10 @@
+# Define the Azure provider
+provider "azurerm" {
+  features {}
+}
+
+# Resource group definition
+resource "azurerm_resource_group" "example_rg" {
+  name     = "example-resources"
+  location = "East US"
+}
